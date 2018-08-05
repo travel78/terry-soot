634ffa0388cdbf564f948a9bae9643d8b8b31020
Yuriy <travel7@i.ua>
2018-08-05 13:29:57 +0300

    added server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+// start server
+const app = express();
+
+app.get('/', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.listen(3000, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(`server is listening on 3000`)
+});
